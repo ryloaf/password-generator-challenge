113bb563741489ebd83d3450889a5977df256fb5
Rylee Jolley <you@example.com>
2024-01-17 13:34:52 -0700

    Added necessary functions and variables in order for the pop-up to appear when I click the generate password button.

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function generatePassword() {
+  var doYouWant= confirm("Do you want")
+  
+  return "password"
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+// Example: Generate a random password of length 12
+const password = generateRandomPassword(12);
+console.log(password);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
